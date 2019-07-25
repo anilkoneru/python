cd937dc9275a2b6e1e8a62f3bffc5110197f5b79
anilkoneru <53311179+anilkoneru@users.noreply.github.com>
2019-07-25 22:46:42 +0530

    Add files via upload

---
+++ week_1.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[2]:
+
+
+x =10
+y =20
+x+y
+
+
+# In[5]:
+
+
+Message = "Hello Python world"
+print(Message)
+
+
+# In[6]:
+
+
+First Name = "Anil"
+Last Name = "Koneru"
+print(first name)
+
+
+# In[7]:
+
+
+First_Name = "Anil"
+Last_Name = "Koneru"
+print(first_name)
+
+
+# In[8]:
+
+
+First_Name = "Anil"
+Last_Name = "Koneru"
+print(First_Name)
+
+
+# In[11]:
+
+
+Name = "tony stark"
+print(Name.title())
+
+
+# In[12]:
+
+
+Name = "tony stark"
+print(Name.upper())
+
+
+# In[13]:
+
+
+name = "TONY STARK"
+print(Name.lower())
+
+
+# In[16]:
+
+
+First_Name = "Anil"
+Last_Name = "Koneru"
+Full_Name = f"{First_Name} {Last_Name}"
+print(Full_Name)
+
+
+# In[17]:
+
+
+First_Name = "anil"
+Last_Name = "koneru"
+Full_Name = f"{First_Name} {Last_Name}"
+print(Full_Name.title())
+
+
+# In[19]:
+
+
+print("anil")
+
+
+# In[20]:
+
+
+print("\tanil")
+
+
+# In[21]:
+
+
+print("anil\nkoneru")
+
+
+# In[23]:
+
+
+First_Name = "Anil "
+Last_Name  = " Koneru"
+print(First_Name.rstrip())
+print(Last_Name.lstrip())
+
+
+# In[ ]:
+
+
+
+
