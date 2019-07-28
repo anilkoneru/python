ed3b25b22ab97601a1025a051bc2d3d7182ab258
anilkoneru <53311179+anilkoneru@users.noreply.github.com>
2019-07-28 20:50:36 +0530

    Add files via upload

---
+++ week_2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Integers
+x,y,z = 2,3,4
+print(x)
+
+
+# In[5]:
+
+
+#constatns should be defined in upper case
+MAX_CONNECTIONS = 100
+Connected = 50
+print("connections remaining =",MAX_CONNECTIONS-Connected)
+
+
+# In[7]:
+
+
+#List
+Colors = ['red','yellow','pink','green']
+print("My favourite color =",Colors)
+
+
+# In[8]:
+
+
+Colors = ['red','yellow','pink','green']
+print("My favourite color =",Colors[1])
+
+
+# In[9]:
+
+
+Colors = ['red','yellow','pink','green']
+print("My favourite color =",Colors[1].title())
+
+
+# In[10]:
+
+
+Colors = ['red','yellow','pink','green']
+print("My favourite color =",Colors[1].upper())
+
+
+# In[ ]:
+
+
+
+
